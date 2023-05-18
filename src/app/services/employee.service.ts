246adb3d73d0e6135468f900b18adaadf26b3d29
Mohammed Abdul Numaan Owais <85054111+manumaanowais@users.noreply.github.com>
2023-05-18 10:01:30 +0530

    Update employee.service.ts

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -10,18 +10,18 @@ export class EmployeeService {
   constructor(private _http:HttpClient) { }
 
   addEmployee(data:any) : Observable<any>{
-    return this._http.post('http://localhost:3000/employees',data)
+    return this._http.post('/.netlify/functions/server/api/employees',data)
   }
 
   getEmployees() : Observable<any>{
-    return this._http.get('http://localhost:3000/employees')
+    return this._http.get('/.netlify/functions/server/api/employees')
   }
 
   updateEmployee(id:number, data:any) : Observable<any>{
-    return this._http.put(`http://localhost:3000/employees/${id}`,data)
+    return this._http.put(`/.netlify/functions/server/api/employees/${id}`,data)
   }
 
   deleteEmployee(id:number) : Observable<any>{
-    return this._http.delete(`http://localhost:3000/employees/${id}`)
+    return this._http.delete(`/.netlify/functions/server/api/employees/${id}`)
   }
 }
